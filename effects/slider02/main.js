0437c6d40c534748a99eb46f372744ebd9c66c66
tommy <tommywhy1989@gmail.com>
2019-07-28 13:28:44 +1200

    slider2 autoplay

--- effects/slider02/main.js
+++ effects/slider02/main.js
@@ -1,4 +1,120 @@
 
+let $buttons = $('#buttonWrapper>button');
+let $slides = $('#images')
+let $images = $slides.children('img')
+let current = 0;
+
+makeFakeSlides($images,$slides)
+
+$slides.hide().offset()
+$slides.css({transform:'translateX(-400px)'}).show()
+
+bindEvents()
+
+
+$('#next').on('click',function () {
+    goToSlide(current +1)
+})
+$('#previous').on('click',function () {
+    goToSlide(current -1)
+})
+let timer = setInterval(function () {
+    goToSlide(current +1)
+},3000)
+$('.container').on('mouseenter',function () {
+    window.clearInterval(timer)
+}).on('mouseleave',function () {
+    timer = setInterval(function () {
+        goToSlide(current +1)
+    },3000)
+})
+
+
+function makeFakeSlides($images,$slides) {
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length-1).clone(true)
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
+function goToSlide(index) {
+    if(index > $buttons.length-1){
+        index = 0
+    }else if(index < 0) {
+        index = $buttons.length -1;
+    }
+    if(current === $buttons.length-1 && index ===0){
+        $slides.css({transform:`translateX(-${($buttons.length+1)*400}px)`}).one('transitionend',function () {
+            $slides.hide().offset()
+            $slides.css({transform:`translateX(-${(index+1)*400}px)` }).show();
+        })
+    }else if(current ===0  && index===$buttons.length-1){
+        console.log(3)
+        $slides.css({transform:`translateX(0)`}).one('transitionend',function () {
+            $slides.hide().offset()
+            $slides.css({transform:`translateX(-${(index+1)*400}px)` }).show();
+        })
+    }else {
+        $slides.css({transform:`translateX(-${(index+1)*400}px)` })
+    }
+    current = index
+}
+
+function bindEvents() {
+
+    //事件委托
+     $('#buttonWrapper').on('click','button',function (e) {
+         let $button = $(e.currentTarget)
+         let index = $button.index()
+         goToSlide(index)
+    })
+    /*$buttons.eq(0).on('click',function (e) {
+        console.log(current)
+        if(current == 2){
+            $slides.css({
+                transform:'translateX(-1600px)'
+            }).one('transitionend',function () {
+                $slides.hide().offset()
+                $slides.css({transform:'translateX(-400px)' }).show();
+            })
+        }else{
+            $slides.css({
+                transform:'translateX(-400px)'
+            })
+            current = 0;
+        }
+    })
+
+    $buttons.eq(1).on('click',function (e) {
+        console.log(current)
+        $slides.css({
+            transform:'translateX(-800px)'
+        })
+        current = 1;
+
+    })
+
+    $buttons.eq(2).on('click',function (e) {
+        console.log(current)
+        if(current == 0){
+            $slides.css({
+                transform:'translateX(0)'
+            }).one('transitionend',function () {
+                $slides.hide().offset()
+                $slides.css({transform:'translateX(-1200px)' }).show();
+            })
+        }
+        $slides.css({
+            transform:'translateX(-1200px)'
+        })
+        current = 2;
+
+    })*/
+}
+
+
+
+
+/*
 let n;
 init();
 let timer = setInterval(()=>{
@@ -56,6 +172,7 @@ function makeLeave($node) {
 function makeEnter($node) {
     $node.addClass('enter').removeClass('current leave')
 }
+*/
 
 /*setTimeout(()=>{
     $('.images > img:nth-child(1)').addClass('leave').removeClass('current').one('transitionend',(e)=>{
