0494f62ed523e498bf16f5ff0dc0a50ddb6d49ee
tommy <tommywhy1989@gmail.com>
2019-08-05 07:55:22 +1200

    mvc

--- js/message.js
+++ js/message.js
@@ -1,36 +1,56 @@
 !function () {
-    var view = document.querySelector('section.message');
-    var controller = {
+    let view = document.querySelector('section.message');
+
+    let model = {
+        initAV:function(){
+            console.log(1)
+            var APP_ID = 'w3uxzEnSEsFqxgNcYgdnkWAU-MdYXbMMI'
+            var APP_KEY = '5IEkYert5bWRhfdHYUWXcyAi'
+            AV.init({appId: APP_ID,appKey: APP_KEY })
+        },
+        //get data
+        fetch:function(){
+            var query = new AV.Query('Message');
+            return query.find() //promise
+        },
+        //save data
+        save:function (username, phone, email, content) { 
+            var  Message = AV.Object.extend('Message');
+            var msg = new Message();
+            msg.set('username',username);
+            msg.set('phone',phone);
+            msg.set('email',email);
+            msg.set('content',content);
+            return msg.save() //promise
+         }
+    }
+
+    let controller = {
       view:null,
+      model:null,
       messageList:null,
-      init:function(view){
+      init:function(view,model){
           this.view = view
-          
+          this.model = model
+          this.model.initAV()
           this.messageList =  document.querySelector('#messageList')
           this.form = document.querySelector('#postMessageForm')
-          this.initAV()
           this.loadMessages()
           this.bindEvents()
         },
-        initAV:function(){
-          console.log(1)
-          var APP_ID = 'w3uxzEnSEsFqxgNcYgdnkWAU-MdYXbMMI'
-          var APP_KEY = '5IEkYert5bWRhfdHYUWXcyAi'
-          AV.init({appId: APP_ID,appKey: APP_KEY })
-        },
+        
         loadMessages:()=>{
-            var query = new AV.Query('Message');
-            query.find()
-                .then((messages)=> {
-                // console.log(1)
-                        let array = messages.map((item)=>{return item.attributes})
-                        array.forEach((item)=>{
-                            let li = document.createElement('li');
-                            li.innerText = `${item.username}: ${item.content} `;
-                            this.messageList.prepend(li)
-                        })
-                    },
-                    (err)=>{alert('submit fail~! please try again...')})
+            /* var query = new AV.Query('Message');
+            query.find() */
+            this.model.fetch().then((messages)=> {
+                let array = messages.map((item)=>{return item.attributes})
+                array.forEach((item)=>{
+                    let li = document.createElement('li');
+                    li.innerText = `${item.username}: ${item.content} `;
+                    this.messageList.prepend(li)
+                })
+            },
+            (err)=>{alert('submit fail~! please try again...')})
             // .then(()=>{},(err)=>{console.log(err)});
 
         },
@@ -40,19 +60,13 @@
                 this.saveMessage()
             })
         },
-        saveMessage: ()=>{
+        saveMessage:function(){
             let myForm = this.form
             let username = myForm.querySelector('input[name=username]').value;
             let phone = myForm.querySelector('input[name=phone]').value;
             let email = myForm.querySelector('input[name=email]').value;
             let content = myForm.querySelector('input[name = content]').value;
-            var  Message = AV.Object.extend('Message');
-            var msg = new Message();
-            msg.set('username',username);
-            msg.set('phone',phone);
-            msg.set('email',email);
-            msg.set('content',content);
-            msg.save().then(function (obj) {
+            this.model.save(username,phone,email,content).then(function (obj) {
                 console.log('save successful~!')
                 // window.location.reload()
                 let li = document.createElement('li');
@@ -70,7 +84,7 @@
 
 
 
-    controller.init(view)
+    controller.init(view,model)
     
 
 
